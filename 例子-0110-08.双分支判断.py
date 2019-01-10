c2f5828c0a02b75479d0ea5498bda09bb8c04feb
blackplusy <happyming07@sina.com>
2019-01-10 15:37:56 +0800

    Add files via upload

---
+++ 例子-0110-08.双分支判断.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+age=int(input('请输入您的芳龄：'))
+
+if age>0 and age<10:
+	print('别打王者了！！！')
+else:
+	print('躲开！我能5杀！！！')
+
---
+++ 例子-0110-09.多分支判断.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+name=input('请输入您的大号：')
+
+if name=='dachui':
+	print('嫁给我！')
+elif	name=='boss':
+	print('你是个好人！')
+else:
+	print('你竟然不如他俩！')
