4e51d0372382a6299b7ee3558e5e186d84e60b6a
blackplusy <happyming07@sina.com>
2019-01-11 11:31:52 +0800

    Add files via upload

---
+++ 例子-0111.06.字符串的补充.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+
+a='python'
+b=' is no.1'
+print(a+b)
+print(a+b[-1])
+
+for i in 'simida':
+	print(i)
+if 't' in a:
+	print('im here!')
+
+print('------------------')
+str1='     python     '
+print(str1.strip())
+print(str1.lstrip())
+print(str1.rstrip())
+
+
+print(len(str1))
+
+
+
+
+
+
+
---
+++ 例子-0111.07.引号.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+print('python')
+
+print("python")
+
+print('''python''')
+
+#print('i'm heygor!!!!')
+
+print("i'm heygor!")
+
+print('i"am simida')
+
+print('''
+	name : big chui
+	age  : 18
+''')
+#三引号主要用于注释，也可以用于多行打印
