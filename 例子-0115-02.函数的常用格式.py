6d8b430f1feea32e339b71701adb11d23ae1a730
blackplusy <happyming07@sina.com>
2019-01-15 10:39:28 +0800

    Add files via upload

---
+++ 例子-0115-02.函数的常用格式.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+def print_name():
+	print('hello world!!')
+
+print_name()
+
+
+def sleep():
+	return 'im sleeping~'
+s=sleep()
+print(s)
+
+
+def sub(x,y):
+	print('x+y=',x+y)
+sub(2,3)
+
+def sub(x,y):
+	return x+y
+res=sub(20,30)
+print(res)
